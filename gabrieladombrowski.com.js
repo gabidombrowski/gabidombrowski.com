2188083c81f6b5bf0f3e033f49cd976e140f127b
Gabi Dombrowski <gabdombrowski@gmail.com>
2019-05-06 17:20:16 -0500

    Update port

--- gabrieladombrowski.com.js
+++ gabrieladombrowski.com.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname));
 
-var port = process.env['PORT'] || 8081;
+var port = process.env['PORT'] || 8080;
 app.listen(port, function() {
     console.log('server listening on port ' + port);
 });
